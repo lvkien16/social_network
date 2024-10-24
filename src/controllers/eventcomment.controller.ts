c5b5ffcce8fae5e9bd87fea5908db03d2b23ba06
kienprovip <lovankien@gmail.com>
2024-10-24 15:23:23 +0700

    like comment

--- src/controllers/eventcomment.controller.ts
+++ src/controllers/eventcomment.controller.ts
@@ -30,7 +30,7 @@ export const getComments = async (req: Request, res: Response, next: NextFunctio
     try {
         const comments = await EventComment.find({ eventId: eventid }).sort({ createdAt: -1 });
 
-        if(!comments) {
+        if (!comments) {
             return next(errorHandler(404, "No comments found"));
         }
 
@@ -44,14 +44,35 @@ export const getReplyComments = async (req: Request, res: Response, next: NextFu
     const { commentid } = req.params;
 
     try {
-        const replyComments =await EventComment.find({ replyingTo: commentid}).sort({ createdAt: -1 });
+        const replyComments = await EventComment.find({ replyingTo: commentid }).sort({ createdAt: -1 });
 
-        if(!replyComments){
+        if (!replyComments) {
             return next(errorHandler(404, "No reply comments found"));
         }
 
         res.status(200).json(replyComments);
 
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const likeComment = async (req: Request, res: Response, next: NextFunction) => {
+    const { commentid, username } = req.params;
+    try {
+        const eventComment = await EventComment.findById(commentid);
+
+        if (!eventComment) {
+            return next(errorHandler(404, "Event's comment not found"));
+        }
+        const userIndex = eventComment.likes.indexOf(username);
+        if (userIndex === -1) {
+            eventComment.likes.push(username);
+        } else {
+            eventComment.likes.splice(userIndex, 1);
+        }
+        await eventComment.save();
+        res.status(200).json(eventComment.likes);
     } catch (error) {
         next(error);
     }
--- src/routes/eventcomment.route.ts
+++ src/routes/eventcomment.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createComment, getComments, getReplyComments } from '../controllers/eventcomment.controller';
+import { createComment, getComments, getReplyComments, likeComment } from '../controllers/eventcomment.controller';
 
 const router = express.Router();
 
 router.post('/create/:eventid/:username', createComment);
 router.get('/get-comment/:eventid', getComments);
 router.get('/get-replycomments/:commentid', getReplyComments);
+router.post('/like-comment/:commentid/:username', likeComment);
 
 export default router;
